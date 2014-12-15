5c791d051db669e4d3cc7bfa01e44db23556fd28
Itang Sanjana <arwata@gmail.com>
2014-12-15 15:56:57 +0700

    Modified for sandbox

--- layers3drop.js
+++ layers3drop.js
@@ -2,10 +2,7 @@
 
 Drupal.behaviors.layers3drop = {
     attach: function (context) {
-        $('.l3d-container').layers3D({
-            transition : 'realtime',
-					responsive : false
-        });
+        $('.l3d-container').layers3D();
     }
 };
 
