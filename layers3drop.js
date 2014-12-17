5f8491d34f64549eae111c7054e3d72f58fe8954
Itang Sanjana <arwata@gmail.com>
2014-12-17 19:59:45 +0700

    Modified

--- layers3drop.js
+++ layers3drop.js
@@ -2,12 +2,7 @@
 
 Drupal.behaviors.layers3drop = {
     attach: function (context) {
-        if ($(window).width() >= 768) {
-            $('.l3d-container').layers3D({
-                transition: 'realtime',
-                responsive: false
-            });
-        }
+        $('.l3d-container').layers3D();
     }
 };
 
