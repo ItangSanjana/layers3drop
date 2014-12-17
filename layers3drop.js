3d966402988260d60888581f388c66c1ac47a26d
Itang Sanjana <arwata@gmail.com>
2014-12-17 19:14:18 +0700

    Tided up

--- layers3drop.js
+++ layers3drop.js
@@ -2,10 +2,12 @@
 
 Drupal.behaviors.layers3drop = {
     attach: function (context) {
-        $('.l3d-container').layers3D({
-            transition : 'realtime',
-					responsive : false
-        });
+        if ($(window).width() >= 768) {
+            $('.l3d-container').layers3D({
+                transition: 'realtime',
+                responsive: false
+            });
+        }
     }
 };
 
